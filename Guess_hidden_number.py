f8934c427467cc5fd35843f8cadb332acd4104e8
Harsha <87359806+20Harsha@users.noreply.github.com>
2021-12-14 18:26:22 +0530

    comments updated in the code

--- Guess_hidden_number.py
+++ Guess_hidden_number.py
@@ -1,7 +1,5 @@
 """
-This a simple game where you have to guess the hidden number.
-Where computer choose the number and that number is not shown to the user
-and user have to guess that number
+This a simple game where you have to guess the hidden number between 1 and 21.
 """
 import random
 print("Welcome to the game")
@@ -9,18 +7,18 @@ print("Lets guess the hidden number but !!!only 9 chances you would get :::")
 while True:
     a1 = input("\nDo you want to continue yes or no : ") #taking user input
     if a1 == "yes":
-        a = random.randrange(1, 21) #choosing random number between 1 to 21 where 1(included) and 21(not included)
+        a = random.randrange(1, 21) #choosing random number 
         count = 1 #initialized the counter to count the number of steps taken
-        while count<=9: #checking the counter
+        while count<=9: 
             n = int(input("\nEnter the number to guess : "))
             if n > a:
                 print("\nGuess a smaller number than this")
                 print("Chances left ", 9 - count) #(9 is total chances) 9 - count tells the user how many chances left
-                count=count+1 #counting how many chances user took
+                count=count+1 
             elif n<a:
                 print("\nGuess number greater than this")
                 print("Chances left ", 9 - count)
-                count=count+1 #counting how many chances user took
+                count=count+1 
 
             elif n==a:
                 print(f"\nHidden number is {a}")
